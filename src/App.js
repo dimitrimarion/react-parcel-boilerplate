f70bb183babaf9b1e95e5ebb5320753085c7d9dc
Dimitri Marion <dimitri.marion@gmail.com>
2019-10-05 20:17:29 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import dog from "@images/dog.jpg";
+
+const App = () => {
+  return (
+    <main className="App">
+      <h1>Hello World!</h1>
+      <img src={dog} alt="" />
+    </main>
+  );
+};
+
+export default App;

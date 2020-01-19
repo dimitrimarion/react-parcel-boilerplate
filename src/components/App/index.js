fd2b3962f2178c03e5b994d3de8acfd3562d7f8e
Dimitri Marion <contact@dimitrimarion.com>
2020-01-19 12:22:01 +0100

    change component folder structure

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import "./index.css";
 import dog from "@images/dog.jpg";
 
 const App = () => {
--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import { render } from "react-dom";
 import "./reset.css";
 import "./index.css";
-import App from "./App";
+import App from "./components/App";
 
 render(<App />, document.getElementById("root"));
